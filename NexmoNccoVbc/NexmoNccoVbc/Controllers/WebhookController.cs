d910f79d52712731525e4ff3571a6a20b6d5c38b
yakovluria <ymluria@gmail.com>
2019-11-06 11:25:31 +0200

    add fallback event

--- NexmoNccoVbc/NexmoNccoVbc/Controllers/WebhookController.cs
+++ NexmoNccoVbc/NexmoNccoVbc/Controllers/WebhookController.cs
@@ -7,7 +7,10 @@ namespace NexmoNccoVbc.Controllers
 {
     public class WebhookController : Controller
     {
-        [AllowAnonymous]
+        private static object obj_locker = new object();
+        private static object obj_locker1 = new object();
+
+        [HttpPost]
         public string NexmoEvents()
         {
             var message = string.Empty;
@@ -16,37 +19,102 @@ namespace NexmoNccoVbc.Controllers
                 message = reader.ReadToEnd();
             }
 
+            lock (obj_locker)
+            {
+                //System.IO.File.AppendAllText(@"C:\tmp\nexmoevent.txt", "Nexmo Events:" + message);
+            }
             return "200";
         }
 
+        [HttpGet]
         public string NexmoAnswer()
         {
-
             var from = Request.QueryString["from"];
             var to = Request.QueryString["to"];
             var legId = Request.QueryString["uuid"];
             var callId = Request.QueryString["conversation_uuid"];
 
-            var response = GetNCCO(from);
+            var response = GetAnswerNCCO(from);
 
             Response.ContentType = "application/json";
             return response;
-
         }
 
-        private string GetNCCO(string clientPhone)
+        [HttpPost]
+        public string NexmoFallback()
+        {
+            var message = string.Empty;
+            using (var reader = new StreamReader(Request.InputStream, Encoding.UTF8))
+            {
+                message = reader.ReadToEnd();
+            }
+
+            lock (obj_locker1)
+            {
+               // System.IO.File.AppendAllText(@"C:\tmp\nexmoevent.txt", "Nexmo Fallback:" + message);
+            }
+
+            var eventJson = JObject.Parse(message);
+
+            var from = (string)eventJson["from"];
+            var status = (string)eventJson["status"];
+
+            if (status != "timeout" &&
+                status != "failed" &&
+                // status != "rejected" ||
+                status != "unanswered" &&
+                status != "busy")
+            {
+                return "200";
+            }
+
+            var response = GetFallbackNCCO(from);
+
+            Response.ContentType = "application/json";
+            return response; 
+        }
+
+        private string GetFallbackNCCO(string from)
+        {
+            var connectCallGroup = new JObject()
+            {
+                new JProperty("action", "connect"),
+                new JProperty("from", $"{from}GG"),
+                new JProperty("timeout", "20"),
+                new JProperty("endpoint", new JArray
+                {
+                    new JObject()
+                    {
+                        new JProperty("type", "vbc"),
+                        new JProperty("extension", "416")
+                    }
+                })
+            }; 
+
+            return new JArray
+            {
+                connectCallGroup,
+            }.ToString();
+        }
+
+        private string GetAnswerNCCO(string clientPhone)
         {
             var welcome = new JObject()
             {
                 new JProperty("action", "talk"),
-                new JProperty("text", "Thank you for calling to Ucallz"),
+                new JProperty("text", "Thank you for calling to Test Nexmo With Ucallz"),
             };
 
             var connectCallQueue = new JObject()
             {
                 new JProperty("action", "connect"),
-                new JProperty("from", $"{clientPhone}Q"),
-                new JProperty("timeout", "15"), 
+                new JProperty("from", $"{clientPhone}QQ"),
+                new JProperty("timeout", "10"),
+                new JProperty("eventType", "synchronous"),
+                new JProperty("eventUrl", new JArray()
+                {
+                    new JValue("http://4537de78.ngrok.io/Webhook/NexmoFallback"),
+                }),
                 new JProperty("endpoint", new JArray
                 {
                     new JObject()
@@ -57,26 +125,10 @@ namespace NexmoNccoVbc.Controllers
                 })
             };
 
-            var connectCallGroup = new JObject()
-            {
-                new JProperty("action", "connect"),
-                new JProperty("from", $"{clientPhone}G"),
-                new JProperty("timeout", "20"),
-                new JProperty("endpoint", new JArray
-                {
-                    new JObject()
-                    {
-                        new JProperty("type", "vbc"),
-                        new JProperty("extension", "416")
-                    }
-                })
-            };
-
             return new JArray
             {
                 welcome,
                 connectCallQueue,
-                connectCallGroup,
             }.ToString();
         }
     }
